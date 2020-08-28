789306d1ce4e42728d8979a1017bdb7462746882
joser <joser.pineal.ai>
2020-08-28 17:35:29 +0800

    发布1.2.0版本

---
+++ CHANGELOG.md
@@ -0,0 +1,59 @@
+# 版本说明
+## 1.2.0版本
+
+修改获取方法设置为可以获取可选参数和确定参数
+
+## 1.2.1版本
+
+修复了`1.2.0`编译问题
+
+## 1.2.0版本
+
+新增设置和获取全局参数的方法
+
+```swift
+/// 设置全局参数
+/// - Parameter block: 设置全局修改器的回掉
+public mutating mutating func set(globaleParameter block: @escaping ((Modify) -> Modify))
+
+/// 获取全局参数
+/// - Parameter key: 参数对应的key
+/// - Parameter default: 默认值
+/// - Returns: 对应类型的值
+public func get<T>(globaleParameter key: String, default: T? = nil) -> T?
+```
+
+
+
+## 1.1.0版本
+
+新增了在注册模块时候可以修改调用模块的参数
+
+```swift
+/// 注册对应的模块
+/// - Parameter controllerType: 模块视图类型
+/// - Parameter block: 可以模块跳转之前在App内部重新修改设置的参数
+public mutating mutating func register<T>(_ controllerType: T.Type, customModify block: ((Modify) -> Modify)? = nil) where T : ControllerCenter.Module
+```
+
+Module新增创建Modify快捷方法
+
+```swift
+extension Module {
+
+    /// 通过调用模块生成前往模块的修改器为了可以获取上个模块的标识符
+    /// - Parameter identifier: 前往模块的标识符
+    /// - Returns: 修改器
+    public static func make(_ identifier: String) -> Modify
+}
+```
+
+Modify新增添加来源模块
+
+```swift
+/// 设置来源模块标识符
+/// - Parameter identifier: 模块标识符
+/// - Returns: 修改器
+public func from(_ identifier: String) -> Modify
+```
+
--- Sources/ControllerCenter/ControllerCenter.swift
+++ Sources/ControllerCenter/ControllerCenter.swift
@@ -2,20 +2,46 @@ import UIKit
 public struct ControllerCenter {
     /**
      创建一个模块的回掉代理
-     - parameter parameter:其他模块传递过来的参数
+     - parameter modify:模块配置
      - returns: 返回该模块的实例对象
      */
-    internal typealias MakeControllerBlock = ((_ parameter:[String:Any]) -> Module)
+    internal typealias MakeControllerBlock = ((_ modify:Modify) -> Module)
     public static var center = ControllerCenter()
     /// 储存已经注册模块的回掉
     internal var registerControllers:[String:MakeControllerBlock] = [:]
+    /// 储存全局参数
+    internal var globalModify:Modify = Modify(identifier: "ControllerCenter")
     /// 注册对应的模块
     /// - Parameter controllerType: 模块视图类型
-    public mutating func register<T:Module>(_ controllerType:T.Type) {
-        let block = { parameter in
-            return T.make(parameter)
+    /// - Parameter block: 可以模块跳转之前在App内部重新修改设置的参数
+    public mutating func register<T:Module>(_ controllerType:T.Type, customModify block:((Modify) -> Modify)? = nil) {
+        let block:((Modify) -> Module) = { modify in
+            if let block = block {
+                return T.make(block(modify).parameter)
+            } else {
+                return T.make(modify.parameter)
+            }
         }
         self.registerControllers[T.identifier] = block
+    }
+    
+    /// 设置全局参数
+    /// - Parameter block: 设置全局修改器的回掉
+    public mutating func set(globaleParameter block:@escaping((Modify) -> Modify)) {
+        globalModify = block(globalModify)
+    }
+    /// 获取全局函数返回可选值
+    /// - Parameter key: 参数对应的key
+    /// - Returns: 返回类型的可选值
+    public func get<T>(globaleParameter key:String) -> T? {
+        return globalModify.parameter[key] as? T
+    }
+    /// 获取全局参数
+    /// - Parameter key: 参数对应的key
+    /// - Parameter default: 默认值
+    /// - Returns: 对应类型的值
+    public func get<T>(globaleParameter key:String, default:T) -> T {
+        return globalModify.parameter[key] as? T ?? `default`
     }
 }
 
